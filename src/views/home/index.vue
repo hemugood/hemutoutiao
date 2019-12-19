5126369e7626d16d30b19324413f4c54d4d737cf
unknown <1983883448@qq.com>
2019-12-19 20:18:40 +0800

    黑马头条PC-主页模块-左侧导航菜单

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,21 @@
 <template>
-  <div>主页</div>
+<el-container>
+  <el-aside style="background-color:#353b4e;height:100vh;width:230px ">
+    <layout-aside></layout-aside>
+  </el-aside>
+  <el-container>
+    <el-header>头部</el-header>
+    <el-main>主要区域</el-main>
+  </el-container>
+</el-container>
 </template>
 
 <script>
+import layoutAside from '../../components/home/layout-aside'
 export default {
-
+  components: {
+    'layout-aside': layoutAside
+  }
 }
 </script>
 
