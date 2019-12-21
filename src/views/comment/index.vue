bf689818b63b8965eb603d6bb8c8320584021397
unknown <1983883448@qq.com>
2019-12-21 22:20:47 +0800

    黑马头条PC-评论列表-打开和关闭评论

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,24 +1,49 @@
 <template>
- <el-card>
-     <bread-crumb slot="headr">
-        <template slot="title">评论列表</template>
-     </bread-crumb>
-     <el-table>
-         <el-table-column label="标题" width="600"></el-table-column>
-         <el-table-column label="评论状态"></el-table-column>
-         <el-table-column label="总评论数"></el-table-column>
-         <el-table-column label="粉丝评论数"></el-table-column>
-         <el-table-column label="操作"></el-table-column>
-     </el-table>
- </el-card>
+  <el-card>
+    <bread-crumb slot="header">
+      <template slot="title">评论列表</template>
+    </bread-crumb>
+    <el-table :data="list">
+      <el-table-column prop="title" label="标题" width="600"></el-table-column>
+      <el-table-column :formatter="formatterBool" prop="comment_status" label="评论状态"></el-table-column>
+      <el-table-column prop="total_comment_count" label="总评论数"></el-table-column>
+      <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="obj">
+          <!-- {{obj.row.comment_status}} -->
+          <el-button type="text">修改</el-button>
+          <el-button type="text">{{obj.row.comment_status?'关闭评论':'打开平论'}}</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      list: []
+    }
+  },
+  methods: {
+    getComment () {
+      this.$axios({
+        url: '/articles',
+        params: { response_type: 'comment' }
+      }).then(result => {
+        this.list = result.data.results
+      })
+    },
+    formatterBool (row, column, cellValue, index) {
+      return cellValue ? '正常' : '关闭'
+    }
+  },
+  created () {
+    this.getComment()
+  }
 }
 </script>
 
 <style>
-
 </style>
