026f41f41fff1e36ab7f00031601622308739a2a
unknown <1983883448@qq.com>
2019-12-23 12:46:14 +0800

    评论列表结束

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-card>
+  <el-card v-loading="loading">
     <bread-crumb slot="header">
       <template slot="title">评论列表</template>
     </bread-crumb>
@@ -11,11 +11,20 @@
       <el-table-column label="操作">
         <template slot-scope="obj">
           <!-- {{obj.row.comment_status}} -->
-          <el-button type="text">修改</el-button>
-          <el-button type="text">{{obj.row.comment_status?'关闭评论':'打开平论'}}</el-button>
+          <el-button type="text" size="small">修改</el-button>
+          <el-button type="text" size="small" @click="openOrClose(obj.row)">{{obj.row.comment_status?'关闭评论':'打开平论'}}</el-button>
         </template>
       </el-table-column>
     </el-table>
+   <el-row type='flex' justify="center" align="middle" style="height:80px">
+      <el-pagination background layout="prev,pager,next"
+      :current-page="page.currentPage"
+      :page-size="page.pageSize"
+      :total='page.total'
+      @current-change="changePage"
+      >
+      </el-pagination>
+   </el-row>
   </el-card>
 </template>
 
@@ -23,20 +32,55 @@
 export default {
   data () {
     return {
-      list: []
+      loading: false,
+      list: [],
+      page: {
+        total: 0,
+        pageSize: 10,
+        currentPage: 1
+      }
     }
   },
   methods: {
+    changePage (newPage) {
+      this.page.currentPage = newPage
+      this.getComment()
+    },
     getComment () {
+      this.loading = true
       this.$axios({
         url: '/articles',
-        params: { response_type: 'comment' }
+        params: { response_type: 'comment', page: this.page.currentPage, per_page: this.page.pageSize }
       }).then(result => {
         this.list = result.data.results
+        this.page.total = result.data.total_count
+        // setTimeout(() => { this.loading = false }, 1000)
+        this.loading = false
       })
     },
     formatterBool (row, column, cellValue, index) {
       return cellValue ? '正常' : '关闭'
+    },
+    openOrClose (row) {
+      let mess = row.comment_status ? '关闭' : '打开'
+      this.$confirm(`你是否确定要${mess}评论吗`).then(() => {
+        this.$axios({
+          method: 'put',
+          url: '/comments/status',
+          params: {
+            articles_id: row.id.toString()
+          },
+          data: {
+            allow_comment: !row.comment_status
+          }
+        }).then(result => {
+          this.$message({
+            type: 'success',
+            message: '操作成功'
+          })
+          this.getComment()
+        })
+      })
     }
   },
   created () {
