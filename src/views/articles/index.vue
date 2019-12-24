a4e70d2e2ddb4c3b932f733ed7dc516429442f8b
unknown <1983883448@qq.com>
2019-12-24 21:31:39 +0800

    主体结构

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -1,47 +1,78 @@
 <template>
-<el-card class="articles">
-    <bread-crumb slot='header'>
-        <template slot='title'>文章列表</template>
+  <el-card class="articles">
+    <bread-crumb slot="header">
+      <template slot="title">文章列表</template>
     </bread-crumb>
     <el-row class="searchTool">
-       <el-col :span="2">
-            <span>文章状态</span>
-       </el-col>
-        <el-col :span="18">
-            <el-radio-group v-model="formData.status">
-            <el-radio :label="5">全部</el-radio>
-            <el-radio :label="0">草稿</el-radio>
-            <el-radio :label="1">待审核</el-radio>
-            <el-radio :label="2">审核通过</el-radio>
-            <el-radio :label="3">审核失败</el-radio>
+      <el-col :span="2">
+        <span>文章状态</span>
+      </el-col>
+      <el-col :span="18">
+        <el-radio-group v-model="formData.status">
+          <el-radio :label="5">全部</el-radio>
+          <el-radio :label="0">草稿</el-radio>
+          <el-radio :label="1">待审核</el-radio>
+          <el-radio :label="2">审核通过</el-radio>
+          <el-radio :label="3">审核失败</el-radio>
         </el-radio-group>
-        </el-col>
+      </el-col>
     </el-row>
     <el-row class="searchTool">
-        <el-col :span="2">
-            <span>频道列表</span>
-        </el-col>
-        <el-col :span="18">
-            <el-select v-model="formData.channel_id">
-                <el-option v-for="item in channels" :key="item.data" :label="item.name" :value="item.id"></el-option>
-            </el-select>
-        </el-col>
+      <el-col :span="2">
+        <span>频道列表</span>
+      </el-col>
+      <el-col :span="18">
+        <el-select v-model="formData.channel_id">
+          <el-option v-for="item in channels" :key="item.data" :label="item.name" :value="item.id"></el-option>
+        </el-select>
+      </el-col>
     </el-row>
     <el-row class="searchTool">
-        <el-col :span="2">
-            <span>时间选择</span>
-        </el-col>
-        <el-col :span="18">
-            <el-date-picker
-            v-model="formData.dataRange"
-                type="daterange"
-                range-separator="-"
-                start-placeholde="开始日期"
-                end-placeholde="结束日期"
-            ></el-date-picker>
-        </el-col>
+      <el-col :span="2">
+        <span>时间选择</span>
+      </el-col>
+      <el-col :span="18">
+        <el-date-picker
+          v-model="formData.dataRange"
+          type="daterange"
+          range-separator="-"
+          start-placeholde="开始日期"
+          end-placeholde="结束日期"
+        ></el-date-picker>
+      </el-col>
     </el-row>
-</el-card>
+    <el-row class="total">
+      <span>共找到1000条符合条件的数据</span>
+    </el-row>
+    <el-row
+      v-for="item in 100"
+      :key="item"
+      class="article-item"
+      type="flex"
+      justify="space-between"
+    >
+      <el-col :span="10">
+        <el-row type="flex">
+          <img src="../../assets/img/404.png" alt />
+          <div class="info">
+            <span>你要是在报错我弄死你</span>
+            <el-tag class="tag">标签一</el-tag>
+            <span class="data">2019-12-24 09:15:42</span>
+          </div>
+        </el-row>
+      </el-col>
+      <el-col :span="6">
+        <el-row class="right" type="flex" justify="end">
+          <span>
+            <i class="el-icon-edit"></i>修改
+          </span>
+          <span>
+            <i class="el-icon-delete">删除</i>
+          </span>
+        </el-row>
+      </el-col>
+    </el-row>
+  </el-card>
 </template>
 
 <script>
@@ -72,10 +103,46 @@ export default {
 </script>
 
 <style lang="less" scoped>
-    .articles{
-        .searchTool{
-            height: 60px;
-            padding-left:50px
-        }
+.articles {
+  .searchTool {
+    height: 60px;
+    padding-left: 50px;
+  }
+  .total {
+      height: 30px;
+    margin: 60px 0;
+    border-bottom: 1px dashed #ccc;
+  }
+  .article-item {
+    margin: 20px 0;
+    padding: 10px 0;
+    border-bottom: 1px solid #f2f3f5;
+    img {
+      width: 180px;
+      height: 100px;
+      margin-right: 10px;
+      border-radius: 4px;
     }
+    .info {
+      height: 100px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      .tag {
+        width: 80px;
+      }
+      .data {
+        color: #999;
+        font-size: 12px;
+      }
+    }
+    .right {
+      span {
+        margin-left: 8px;
+        font-size: 14px;
+        cursor: pointer;
+      }
+    }
+  }
+}
 </style>
