a548d6d697c8c0a143c4d2bc15d1ae6cf68156e6
unknown <1983883448@qq.com>
2019-12-29 09:32:51 +0800

    引入进度条

--- src/permision/index.js
+++ src/permision/index.js
@@ -1,5 +1,8 @@
 import router from '../router'
+import progress from 'nprogress'
+import 'nprogress/nprogress.css'
 router.beforeEach(function (to, from, next) {
+  progress.start()
   if (to.path.startsWith('/home')) {
     let token = window.localStorage.getItem('user-token')
     if (token) {
@@ -10,4 +13,8 @@ router.beforeEach(function (to, from, next) {
   } else {
     next()
   }
+})
+router.afterEach(function () {
+  progress.done()
+  // setTimeout(() => progress.done(), 500)
 })
